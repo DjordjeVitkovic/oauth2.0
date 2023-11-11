d2ebebb4699d1a722730d900b7fccc20883e6b69
Djordje <djordjev9@gmail.com>
2023-11-11 01:14:22 +0100

    Add @Post method and should be tested via browser

--- resource-service/src/main/java/com/resource/controller/DemoController.java
+++ resource-service/src/main/java/com/resource/controller/DemoController.java
@@ -2,8 +2,12 @@ package com.resource.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("resource")
@@ -12,5 +16,10 @@ public class DemoController {
   @GetMapping("/demo")
   public ResponseEntity<String> demo() {
     return ResponseEntity.ok("Demo from resource");
+  }
+
+  @PostMapping("/demo")
+  public ResponseEntity<Map<String, String>> postDemo() {
+    return ResponseEntity.ok((Map<String, String>) new HashMap<>().put("demo", "demo"));
   }
 }
